fcdfbfcf9ac20efbd88be89976e1ff9f5af0fccf
George <ballard.george@gmail.com>
2020-02-29 10:17:32 -0800

    added spot1 replacing with X

--- tictactoe.py
+++ tictactoe.py
@@ -14,7 +14,7 @@ Who are our lovely contestants?'''.format(spot1=gameboard[1], spot2=gameboard[2]
     
     player1 = raw_input("Player 1? \n")
     player2 = raw_input("Player 2? \n")
-    
+    spot1, spot2, spot3, spot4, spot5, spot6, spot7, spot8, spot9 = gameboard[1], gameboard[2], gameboard[3], gameboard[4], gameboard[5], gameboard[6], gameboard[7], gameboard[8], gameboard[9]
     system("cls")
     print ("Wonderful!! {} goes first!\n".format(player1))
     while 1:
@@ -25,8 +25,10 @@ Who are our lovely contestants?'''.format(spot1=gameboard[1], spot2=gameboard[2]
  {spot4} | {spot5} |{spot6}
 ===========
  {spot7} | {spot8} |{spot9}
-        '''.format(spot1=gameboard[1], spot2=gameboard[2], spot3=gameboard[3], spot4=gameboard[4], spot5=gameboard[5], spot6=gameboard[6], spot7=gameboard[7], spot8=gameboard[8], spot9=gameboard[9]))
+        '''.format(spot1=spot1, spot2=spot2, spot3=spot3, spot4=spot4, spot5=spot5, spot6=spot6, spot7=spot7, spot8=spot8, spot9=spot9))
         num = int(raw_input("Which square do you want to pick? \n"))
+        if num == 1:
+            spot1="X"
         if num == 12:
             break
     
